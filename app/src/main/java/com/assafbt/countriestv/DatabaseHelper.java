0a6a57870a87dfed4dd52fbb28b387883f688d99
assaf <assafbt@gmail.com>
2020-01-19 20:42:21 +0200

    add: DatabaseHelper - save the require data with SQLite, sort by name/area MainActivity - show data on RecyclerView,add sort buttons for name and area columns

---
+++ app/src/main/java/com/assafbt/countriestv/DatabaseHelper.java
@@ -0,0 +1,208 @@
+package com.assafbt.countriestv;
+/*
+ * *
+ *  * Created by Assaf Biton
+ *
+ */
+
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    private String TAG = "DatabaseHelper";
+
+    // Database Version
+    private static final int DATABASE_VERSION = 1;
+
+    // Database Name
+    private static final String DATABASE_NAME = "country_db";
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }//DatabaseHelper(Context
+
+    // Creating Tables
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        // create notes table
+        sqLiteDatabase.execSQL(Country.CREATE_TABLE);
+        Log.i(TAG, "isDatabaseIntegrityOk " + sqLiteDatabase.isDatabaseIntegrityOk());
+    }//onCreate(SQLiteDatabase
+
+    // Upgrading database
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        // Drop older table if existed
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + Country.TABLE_NAME);
+
+        // Create tables again
+        onCreate(sqLiteDatabase);
+    }//onUpgrade(SQLiteDatabase
+
+    public long insertCountry(Country country) {
+        // get writable database as we want to write data
+        SQLiteDatabase db = this.getWritableDatabase();
+        Log.i(TAG+" insertCountry,", "db = " + db.toString());
+
+        ContentValues values = new ContentValues();
+
+        values.put(Country.COLUMN_NAME, country.getName());
+        values.put(Country.COLUMN_NATIVE_NAME, country.getNativeName());
+        values.put(Country.COLUMN_AREA, country.getArea());
+
+        // insert row
+        long id = db.insert(Country.TABLE_NAME, null, values);
+        Log.i(TAG +" insertCountry,", "added in possition: " + id);
+        Log.i(TAG +" insertCountry, value - ", values.toString());
+
+        // close db connection
+        db.close();
+
+        // return newly inserted row id
+        return id;
+    }//insertCountry
+
+    public List<Country> getAllCountries() { // #################
+        String funTag = TAG + ", getAllCountries:";
+        String query =  "SELECT  * FROM " + Country.TABLE_NAME ;
+
+        List<Country> mCountryList = new ArrayList<>();
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(query, null);
+        Country mCountry;
+
+        if (cursor.moveToFirst()) {
+            do {
+                mCountry = new Country();
+                mCountry.setName(cursor.getString(cursor.getColumnIndex(Country.COLUMN_NAME)));
+                mCountry.setNativeName(cursor.getString(cursor.getColumnIndex(Country.COLUMN_NATIVE_NAME)));
+                mCountry.setArea(cursor.getInt(cursor.getColumnIndex(Country.COLUMN_AREA)));
+
+                Log.i(funTag, "mCountry.name -" + mCountry.name );
+                Log.i(funTag, "mCountry.nativeName -" + mCountry.nativeName );
+                Log.i(funTag, "mCountry.area -" + mCountry.area );
+
+                mCountryList.add(mCountry);
+            } while (cursor.moveToNext());
+        }
+        // close db connection
+        db.close();
+
+        return mCountryList;
+    }//getAllCountries
+
+    public List<Country> getSortedCountries(String columName, String sortBy) { // #################
+        String funTag = TAG + ", getAllCountries:";
+        String query =  "";
+        switch (columName)
+        {
+            case "name":
+                query =  "SELECT  * FROM " + Country.TABLE_NAME + " ORDER BY " + Country.COLUMN_NAME + " " + sortBy;
+                break;
+            case "area":
+                query =  "SELECT  * FROM " + Country.TABLE_NAME + " ORDER BY " + Country.COLUMN_AREA + " " + sortBy;
+                break;
+            case "none":
+                query =  "SELECT  * FROM " + Country.TABLE_NAME;
+                break;
+            default:
+                query =  "SELECT  * FROM " + Country.TABLE_NAME;
+                break;
+        }
+
+        List<Country> mCountryList = new ArrayList<>();
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(query, null);
+        Country mCountry;
+
+        if (cursor.moveToFirst()) {
+            do {
+                mCountry = new Country();
+                mCountry.setName(cursor.getString(cursor.getColumnIndex(Country.COLUMN_NAME)));
+                mCountry.setNativeName(cursor.getString(cursor.getColumnIndex(Country.COLUMN_NATIVE_NAME)));
+                mCountry.setArea(cursor.getInt(cursor.getColumnIndex(Country.COLUMN_AREA)));
+
+                Log.i(funTag, "mCountry.name -" + mCountry.name );
+                Log.i(funTag, "mCountry.nativeName -" + mCountry.nativeName );
+                Log.i(funTag, "mCountry.area -" + mCountry.area );
+
+                mCountryList.add(mCountry);
+            } while (cursor.moveToNext());
+        }
+        // close db connection
+        db.close();
+
+        return mCountryList;
+    }//getAllCountries
+
+    public List<Country> getAllCountriesSortedByName(String sort) { // #################
+        String funTag = TAG + ", getAllCountriesSortedByName:";
+
+        String query =  "SELECT  * FROM " + Country.TABLE_NAME + " ORDER BY " + Country.COLUMN_NAME + " " + sort;
+
+        List<Country> mCountryList = new ArrayList<>();
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(query, null);
+        Country mCountry;
+
+        if (cursor.moveToFirst()) {
+            do {
+                mCountry = new Country();
+                mCountry.setName(cursor.getString(cursor.getColumnIndex(Country.COLUMN_NAME)));
+                mCountry.setNativeName(cursor.getString(cursor.getColumnIndex(Country.COLUMN_NATIVE_NAME)));
+                mCountry.setArea(cursor.getInt(cursor.getColumnIndex(Country.COLUMN_AREA)));
+
+                mCountryList.add(mCountry);
+            } while (cursor.moveToNext());
+        }
+        // close db connection
+        db.close();
+
+        return mCountryList;
+    }//getAllCountriesSortedByName
+
+    public List<Country> getAllCountriesSortedByArea(String sort) { // #################
+        String funTag = TAG + ", getAllCountriesSortedByName:";
+
+        String query =  "SELECT  * FROM " + Country.TABLE_NAME + " ORDER BY " + Country.COLUMN_AREA + " " + sort;
+
+        List<Country> mCountryList = new ArrayList<>();
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(query, null);
+        Country mCountry;
+
+        if (cursor.moveToFirst()) {
+            do {
+                mCountry = new Country();
+                mCountry.setName(cursor.getString(cursor.getColumnIndex(Country.COLUMN_NAME)));
+                mCountry.setNativeName(cursor.getString(cursor.getColumnIndex(Country.COLUMN_NATIVE_NAME)));
+                mCountry.setArea(cursor.getInt(cursor.getColumnIndex(Country.COLUMN_AREA)));
+
+                mCountryList.add(mCountry);
+            } while (cursor.moveToNext());
+        }
+        // close db connection
+        db.close();
+
+        return mCountryList;
+    }//getAllCountriesSortedByArea
+
+    public int getDatabaseVersion()
+    {
+        return this.DATABASE_VERSION;
+    }
+
+    public String getDatabaseName()
+    {
+        return this.DATABASE_NAME;
+    }
+
+}//DatabaseHelper extends
--- app/src/main/java/com/assafbt/countriestv/MainActivity.java
+++ app/src/main/java/com/assafbt/countriestv/MainActivity.java
@@ -1,14 +1,165 @@
 package com.assafbt.countriestv;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    String mUrl = "https://restcountries.eu/rest/v2/all";
+
+    private RecyclerView mList;
+    private String TAG = "MainActivity";
+    private LinearLayoutManager linearLayoutManager;
+    private DividerItemDecoration dividerItemDecoration;
+    private List<Country> countryList = new ArrayList<>();
+    private RecyclerView.Adapter adapter;
+    private DatabaseHelper dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String functionTAG = TAG +" onCreate";
+
+        dbHelper = new DatabaseHelper(this);
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Loading...");
+        progressDialog.show();
+        fetchData();
+
+        mList = findViewById(R.id.main_list);
+
+        countryList = new ArrayList<>();
+        countryList = dbHelper.getSortedCountries("none","");
+
+        Log.i(functionTAG, "DatabaseVersion " + dbHelper.getDatabaseVersion());
+
+        progressDialog.dismiss();
+
+        final Button btnSortName, btnSortArea;
+        btnSortName = (Button)findViewById(R.id.btn_sort_name);
+        btnSortName.setText("name A->Z");
+        btnSortName.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                countryList.clear();
+                String order = "ASC";
+                if (btnSortName.getText() == "name A->Z") {
+                    order = "DESC";
+                    btnSortName.setText("name Z->A");
+                }
+                else {
+                    order = "ASC";
+                    btnSortName.setText("name A->Z");
+                }
+
+                countryList = dbHelper.getSortedCountries("name", order);
+                updateRv();
+            }
+        });
+
+        btnSortArea = (Button)findViewById(R.id.btn_sort_area);
+        btnSortArea.setText("Area A->Z");
+        btnSortArea.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                countryList.clear();
+                String order = "ASC";
+                if (btnSortArea.getText() == "Area A->Z") {
+                    order = "DESC";
+                    btnSortArea.setText("Area Z->A");
+                }
+                else {
+                    order = "ASC";
+                    btnSortArea.setText("Area A->Z");
+                }
+
+                countryList = dbHelper.getSortedCountries("area", order);
+                updateRv();
+            }
+        });
+
+        updateRv();
+
+    }//onCreate
+
+    private void fetchData() {
+        final String functionTAG = TAG +" fetchData";
+
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(mUrl, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                for (int i = 0; i < response.length(); i++) {
+                    try {
+                        JSONObject jsonObject = response.getJSONObject(i);
+                        Log.i(functionTAG,"########### jsonObject " + i + " ########### ");
+                        Log.i(functionTAG,"jsonObject " + jsonObject.toString());
+
+                        String mName = jsonObject.getString("name");
+                        Log.i(functionTAG,"mName " + mName);
+                        String mNativeName = jsonObject.getString("nativeName");
+                        Log.i(functionTAG,"mNativeName " + mNativeName);
+                        int mArea = jsonObject.getInt("area");
+                        Log.i(functionTAG,"area " + mArea);
+                        long possition = dbHelper.insertCountry(new Country(mName, mNativeName,mArea));
+
+                        Country country = new Country("0","0",0);
+                        country.setName(mName);
+                        Log.i(functionTAG,"getName()" + country.getName());
+                        country.setNativeName(jsonObject.getString("nativeName"));
+                        Log.i(functionTAG,"getNativeName()" + country.getNativeName());
+                        country.setArea(jsonObject.getInt("area"));
+                        Log.i(functionTAG,"getArea()" + country.getArea());
+
+                        // long possition = dbHelper.insertCountry(country);
+
+                        Log.i(functionTAG, "added in possition: " + possition);
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                        Log.e("Error on JSONException", e.toString());
+                    }
+                }
+                adapter.notifyDataSetChanged();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("Error on Volley", error.toString());
+            }
+        });
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(jsonArrayRequest);
+    }//fetchData
+
+    private void updateRv ()
+    {
+        adapter = new CountryAdapter(this,countryList);
+
+        linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        dividerItemDecoration = new DividerItemDecoration(mList.getContext(), linearLayoutManager.getOrientation());
+
+        mList.setHasFixedSize(true);
+        mList.setLayoutManager(linearLayoutManager);
+        mList.addItemDecoration(dividerItemDecoration);
+        mList.setAdapter(adapter);
     }
-}
+}//MainActivity
